91903687ce2e8baf228f8a8e3ea05a0b25e5b232
parsa_moradi <parsamoradi0605@gmail.com>
2023-09-06 01:51:21 +0330

    save data

--- main.py
+++ main.py
@@ -1,45 +1,99 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import openpyxl
+from datetime import datetime
+from selenium.webdriver.firefox.options import Options
 
-# ایجاد یک نسخه از مرورگر Firefox
-driver = webdriver.Firefox()
 
-# دامنه و واژه کلیدی مورد نظر را تعیین کنید
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+
+
+
+# Create an instance of the Firefox web browser
+
+driver = webdriver.Firefox(options=options)
+
+# Define the domain and keyword you want to search for
 domain = "sdata.ir"
-keyword = "اس دیتا"
+# Define the list of keywords you want to search for
+keywords = ["شرکت تحلیل داده", "شرکت فناوری اطلاعات", "اس دیتا","بیگ دیتا","تحلیل داده"]
 
-# ترکیب دامنه و واژه کلیدی برای جستجو در گوگل
-search_query = keyword
 
-# آدرس صفحه گوگل را باز کنید
-driver.get("https://www.google.com/search?q=" + search_query)
 
-# دریافت نتایج جستجو
-search_results = driver.find_elements(By.CSS_SELECTOR, ".tF2Cxc")
+# Specify the number of pages to check (5 pages in this example)
+pages_to_check = 7
 
-# شمارنده برای رتبه دامنه
-rank = 1
 
-# پیدا کردن دامنه مورد نظر و چاپ رتبه و صفحه آن
-for result in search_results:
-    # گرفتن عنوان لینک
-    link_title = result.find_element(By.CSS_SELECTOR, "h3").text
+# Create a new Excel workbook and add a worksheet
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+worksheet.title = "Search Results"
 
-    # گرفتن آدرس لینک
-    link_url = result.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+# Add headers to the worksheet
+worksheet["A1"] = "Keyword"
+worksheet["B1"] = "Page"
+worksheet["C1"] = "Rank"
+worksheet["D1"] = "Link Address"
+worksheet["E1"] = "Link TITLE"
 
-    # بررسی آیا دامنه مورد نظر در آدرس لینک وجود دارد
-    if domain in link_url:
-        print(f"score: {rank}")
-        print(f"link name: {link_title}")
-        print(f":link addres {link_url}\n")
+# Set column widths (adjust the values as needed)
+worksheet.column_dimensions['A'].width = 50  # Column A width
+worksheet.column_dimensions['B'].width = 5  # Column B width
+worksheet.column_dimensions['C'].width = 5  # Column C width
+worksheet.column_dimensions['D'].width = 60  # Column D width
+worksheet.column_dimensions['E'].width = 40  # Column D width
+# Initialize the page number to 0
+page = 0
+rank=0
 
-    else:
-    #   print(result.text)
-        print("didnt find----------------------")
-    # اگر رتبه دامنه پیدا شد، می‌توانید از دستور break استفاده کنید تا حلقه خاتمه یابد.
-    # if domain in link_url:
-    #     break
-    rank += 1
-# بستن مرورگر بعد از اتمام
+# Get the current date and time
+current_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+for keyword in keywords:
+    page = 0  # Reset page to 0 for each keyword search
+    not_found_pages = 0  # Initialize the variable for counting not found pages for each keyword
+
+    while page < pages_to_check:
+        # Increment the page number before opening the page
+
+        # Open the Google search page with the formulated search query
+        driver.get(f"https://www.google.com/search?q={keyword}&start={(page - 1) * 10}")
+        page += 1
+        # Find and store the search results on the page using a CSS selector
+        search_results = driver.find_elements(By.CSS_SELECTOR, ".tF2Cxc")
+
+        found = False
+        rank = 0
+
+        # Iterate through the search results to find the domain and print its rank and details
+        for result in search_results:
+            rank += 1
+
+            link_title = result.find_element(By.CSS_SELECTOR, "h3").text
+            link_url = result.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+
+            if domain in link_url:
+                print(f"page : {page}")
+                print(f"Rank: {rank}")
+                # print(f"Link name: {link_title}")
+                print(f"Link address: {link_url}\n")
+                found = True
+                row = [keyword,page ,rank,link_url,link_title ]
+                worksheet.append(row)
+
+        if not found:
+            print(f"Domain not found on page {page}.")
+            not_found_pages += 1
+
+        # اگر کلمه کلیدی در تمام 5 صفحه پیدا نشده باشد، یک رکورد خالی به اکسل اضافه کنید
+        if not_found_pages == pages_to_check:
+            print(f"Keyword '{keyword}' not found on any page.")
+            row = [keyword, "نتیجه‌ای پیدا نشد در 5 صفحه اول", "", ""]
+            worksheet.append(row)
+workbook.save(f"search_results{keyword}-{current_date}.xlsx")
+
+    # Increment the rank counter
+
+# Close the web browser when done
 driver.quit()
--- test.py
+++ test.py
@@ -1,55 +1,97 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import openpyxl
+from datetime import datetime
+from selenium.webdriver.firefox.options import Options
+
+
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+
+
 
 # Create an instance of the Firefox web browser
-driver = webdriver.Firefox()
+
+driver = webdriver.Firefox(options=options)
 
 # Define the domain and keyword you want to search for
 domain = "sdata.ir"
-keyword = "شرکت تحلیل داده"
+# Define the list of keywords you want to search for
+keywords = ["شرکت تحلیل داده", "شرکت فناوری اطلاعات", "اس دیتا","بیگ دیتا","تحلیل داده"]
 
-# Formulate the search query by combining the domain and keyword
-search_query = keyword
-
-# Initialize the rank counter for the domain
 
 
 # Specify the number of pages to check (5 pages in this example)
-pages_to_check = 5
+pages_to_check = 7
 
+
+# Create a new Excel workbook and add a worksheet
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+worksheet.title = "Search Results"
+
+# Add headers to the worksheet
+worksheet["A1"] = "Keyword"
+worksheet["B1"] = "Page"
+worksheet["C1"] = "Rank"
+worksheet["D1"] = "Link Address"
+worksheet["E1"] = "Link TITLE"
+
+# Set column widths (adjust the values as needed)
+worksheet.column_dimensions['A'].width = 50  # Column A width
+worksheet.column_dimensions['B'].width = 5  # Column B width
+worksheet.column_dimensions['C'].width = 5  # Column C width
+worksheet.column_dimensions['D'].width = 60  # Column D width
+worksheet.column_dimensions['E'].width = 40  # Column D width
 # Initialize the page number to 0
 page = 0
 rank=0
-while page < pages_to_check:
-    # Increment the page number before opening the page
 
+# Get the current date and time
+current_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+for keyword in keywords:
+    page = 0  # Reset page to 0 for each keyword search
+    not_found_pages = 0  # Initialize the variable for counting not found pages for each keyword
 
-    # Open the Google search page with the formulated search query
-    driver.get(f"https://www.google.com/search?q={search_query}&start={(page - 1) * 10}")
-    page += 1
-    # Find and store the search results on the page using a CSS selector
-    search_results = driver.find_elements(By.CSS_SELECTOR, ".tF2Cxc")
+    while page < pages_to_check:
+        # Increment the page number before opening the page
 
-    found = False
-    rank = 0
+        # Open the Google search page with the formulated search query
+        driver.get(f"https://www.google.com/search?q={keyword}&start={(page - 1) * 10}")
+        page += 1
+        # Find and store the search results on the page using a CSS selector
+        search_results = driver.find_elements(By.CSS_SELECTOR, ".tF2Cxc")
 
-    # Iterate through the search results to find the domain and print its rank and details
-    for result in search_results:
-        rank += 1
+        found = False
+        rank = 0
 
-        link_title = result.find_element(By.CSS_SELECTOR, "h3").text
-        link_url = result.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+        # Iterate through the search results to find the domain and print its rank and details
+        for result in search_results:
+            rank += 1
 
-        if domain in link_url:
-            print(f"Rank: {rank}")
-            print(f"Link name: {link_title}")
-            print(f"Link address: {link_url}")
-            print(f"page : {page}\n")
-            found = True
+            link_title = result.find_element(By.CSS_SELECTOR, "h3").text
+            link_url = result.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
 
-    if not found:
-        print(f"Domain not found on page {page}.")
+            if domain in link_url:
+                print(f"page : {page}")
+                print(f"Rank: {rank}")
+                # print(f"Link name: {link_title}")
+                print(f"Link address: {link_url}\n")
+                found = True
+                row = [keyword,page ,rank,link_url,link_title ]
+                worksheet.append(row)
+
+        if not found:
+            print(f"Domain not found on page {page}.")
+            not_found_pages += 1
+
+        # اگر کلمه کلیدی در تمام 5 صفحه پیدا نشده باشد، یک رکورد خالی به اکسل اضافه کنید
+        if not_found_pages == pages_to_check:
+            print(f"Keyword '{keyword}' not found on any page.")
+            row = [keyword, "نتیجه‌ای پیدا نشد در 5 صفحه اول", "", ""]
+            worksheet.append(row)
+workbook.save(f"search_results{keyword}-{current_date}.xlsx")
 
     # Increment the rank counter
 
